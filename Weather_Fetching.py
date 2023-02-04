c2c25670a1f72220444f9a8e4e237a66509763a4
AaTekle <108105300+AaTekle@users.noreply.github.com>
2023-02-04 09:19:44 -0500

    Update Weather_Fetching.py

--- Weather_Fetching.py
+++ Weather_Fetching.py
@@ -7,8 +7,8 @@ API_Key = '905cff5fdb5814434f5e47644cfd5476'
 
 City = input("Enter any city: ")
 
-URL = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}" + API_Key + "&q=" + City
+url = "https://api.openweathermap.org/data/2.5/weather?appid=" + API_Key + "&q=" + City
 
-weather_data = requests.get(URL).json()
+weather_data = requests.get(url).json()
 
 pprint(weather_data)
